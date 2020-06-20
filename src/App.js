c5e21ceaaec32c775cb2bf01e6db8e0e41fc4b3e
Hasbi Musaddad <musaddadhasbi@gmail.com>
2020-06-20 07:44:56 +0700

    [hasbi] nambah komponen
    komponen dropdown, channels card "koyone smart sih" page channels

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import HomePage from "./components/pages/home/Home";
 import SeriesPage from "./components/pages/series/Series";
-
+import Channels from "./components/pages/channels";
 
 const App = () => {
   return (
-
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/channels" exact component={Channels} />
           <Route path="/series">
             <SeriesPage />
           </Route>
@@ -27,6 +22,6 @@ const App = () => {
       </div>
     </Router>
   );
-}
+};
 
 export default App;
